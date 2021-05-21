4775a8cb7f63c0a17ab2d55a2bcfbf48ee710a95
Jacob Jelen <jacobjelen@gmail.com>
2021-05-21 14:30:51 +0100

    Bare bones. Button in popup sends a message to content script of a current tab, calls a function.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+// POPUP //
+
+/*global chrome*/    //enables Chrome API. 
+// Alternatively /* eslint-disable no-undef */     // https://codepen.io/supernova_at/post/creating-a-browser-extension-with-create-react-app
 
 import './App.css';
 
+// POPUP WINDOW //////////////////////////////////////
 function App() {
+  const mes = 'refresh';
+
   return (
-    <div className="App" >
+    <div className="body" >
       <div className="eclipser">hello</div>
+      <button onClick={() => { messageCurrentTab(mes)} }>message</button>
     </div>
   );
 }
 
 export default App;
+
+// FUNCTIONS //////////////////////////////////////
+function messageCurrentTab(message){
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {			// message the current website / active tab
+    chrome.tabs.sendMessage(tabs[0].id, message);
+  });//tabs query
+}
+
+
