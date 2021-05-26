aa14231db80464ab857b3ff6ee9ed0f50a8066c2
Jacob Jelen <jacobjelen@gmail.com>
2021-05-26 13:13:34 +0100

    Added basic icons

--- src/components/Domain.js
+++ src/components/Domain.js
@@ -1,13 +1,36 @@
 import SetList from "./SetList";
 
+//https://react-icons.github.io/react-icons/icons?name=ai
+import { 
+    AiOutlineMenu,                 // menu
+    AiOutlineSetting,                // settings
+    AiOutlineCloseCircle,        //delete
+    
+    AiOutlineEyeInvisible,       //invisible
+    AiOutlineEye,                //visible
+    
+    AiOutlineDown,               //expand down
+    AiOutlineUp,
+    
+    AiOutlineClockCircle,       // clock
+} from "react-icons/ai";     
+
+
 const Domain = ({domainName, domainSettings}) => {
 
     return (
         <div className="domainDiv">
             <div className="domainLine">
-                <div className="domainName">{domainName}</div>
+                <div className="domainName">
+                    {domainSettings.active ? <AiOutlineEye style={{color:"green"}}/> : <AiOutlineEyeInvisible style={{color:"grey"}}/>}
+                    
+                    {domainName}
+                    </div>
             
-                <div className="domainButtons"> X </div>
+                
+                <div className="domainButtons"> 
+                <AiOutlineCloseCircle />
+                </div>
             
             </div> 
             
